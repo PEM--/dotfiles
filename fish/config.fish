1fa05dd509c6541ffe45e191419c2a3ebfc26e9c
PEM-- <pemarchandet@gmail.com>
2014-06-18 01:00:35 +0200

    Fish prompt curated

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,15 @@
+# Regular Oh-My-Fish directory
 set fish_path $HOME/.oh-my-fish
-set fish_theme idan
+# Custom Fish's plugin
+set fish_custom $HOME/dotfiles/oh-my-fish
+# Theme
+set fish_theme pem
 # Oh-My-Fish's plugins
 set fish brew extract node percol vundle tmux z
-# Custom Fish's plugin
-#set fish_custom $HOME/dotfiles/oh-my-fish
 . $fish_path/oh-my-fish.fish
 # Remove Fish's greeting
 set -g -x fish_greeting ''
 # Aliases
 alias vim="mvim -v"
 alias vi="mvim -v"
+alias l="ls"
