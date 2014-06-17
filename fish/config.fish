b279d8c3d8057916203ec5fc2b72f4bfe59be4c9
PEM-- <pemarchandet@gmail.com>
2014-06-18 00:21:09 +0200

    Going fishy

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,12 @@
-set -g -x PATH /usr/local/bin $PATH
+set fish_path $HOME/.oh-my-fish
+set fish_theme idan
+# Oh-My-Fish's plugins
+set fish brew extract node percol vundle tmux z
+# Custom Fish's plugin
+#set fish_custom $HOME/dotfiles/oh-my-fish
+. $fish_path/oh-my-fish.fish
+# Remove Fish's greeting
+set -g -x fish_greeting ''
+# Aliases
+alias vim="mvim -v"
+alias vi="mvim -v"
