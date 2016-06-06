aa7cacdf76c697da263d3a18af37a62392b4af66
PEM-- <pemarchandet@gmail.com>
2016-06-06 16:14:53 +0200

    Relax ESLint rules

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,5 @@ set -g OMF_CONFIG $HOME/.config/omf
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
